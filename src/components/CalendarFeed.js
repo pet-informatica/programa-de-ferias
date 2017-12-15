692e9951413448d53440b38a64eb5c3eba6bed57
Trakton <higor.cmbotelho@gmail.com>
2017-12-15 14:42:49 -0300

    Updating calendar

--- src/components/CalendarFeed.js
+++ src/components/CalendarFeed.js
@@ -15,7 +15,7 @@ export default class HistoryFeed extends Component{
             </Grid.Row>
             <Grid.Row>
                 <Grid.Column floated='right'>
-                    <Card align='right' header='12, Janeiro, 2018' description='Fim das Inscrições'></Card>
+                    <Card align='right' header='15, Janeiro, 2018' description='Fim das Inscrições'></Card>
                 </Grid.Column>
                 <Grid.Column floated='left'>
                 </Grid.Column>
@@ -25,12 +25,12 @@ export default class HistoryFeed extends Component{
                     <Icon name="calendar" size="huge"></Icon>
                 </Grid.Column>
                 <Grid.Column>
-                    <Card align='left' header='15, Janeiro, 2018' meta='das 9h às 12h, diariamente' description='Início das Aulas'></Card>
+                    <Card align='left' header='22, Janeiro, 2018' meta='das 9h às 12h, diariamente' description='Início das Aulas'></Card>
                 </Grid.Column>
             </Grid.Row>
             <Grid.Row>
                 <Grid.Column floated='right'>
-                    <Card align='right' header='29, Janeiro, 2018' description='Fim das Aulas'></Card>
+                    <Card align='right' header='02, Janeiro, 2018' description='Fim das Aulas'></Card>
                 </Grid.Column>
                 <Grid.Column floated='left'>
                 </Grid.Column>
@@ -40,7 +40,7 @@ export default class HistoryFeed extends Component{
                     <Icon name="graduation" size="huge" flipped='horizontally'></Icon>
                 </Grid.Column>
                 <Grid.Column>
-                    <Card align='left' header='05, Fevereiro, 2018' description='Divulgação das Notas'></Card>
+                    <Card align='left' header='08, Fevereiro, 2018' description='Divulgação das Notas'></Card>
                 </Grid.Column>
             </Grid.Row>
         </Grid>
